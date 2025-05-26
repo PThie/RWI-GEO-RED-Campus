858c54bcf5315e52c91db696ff3343b4f0093dd7
Patrick Thiel <patrickthiel@outlook.de>
2025-05-26 14:55:09 +0200

    Create helpers_target_names.R
    Function to create target object names that are used in the tar_eval process.

---
+++ src/helpers/helpers_target_names.R
@@ -0,0 +1,29 @@
+helpers_target_names <- function() {
+    #' @title Create target names
+    #' 
+    #' @description This function creates a list of target names used in the
+    #' pipeline when dynamic branching is used (i.e. when tar_eval is used).
+    #'  
+    #' @return List, target names
+    #' @author Patrick Thiel
+    
+    #--------------------------------------------------
+    # list of target names
+
+    ##### General names
+    static_housing_types <- c("WK", "HK", "WM")
+
+    target_names <- list(
+        "static_housing_types" = static_housing_types,
+        #--------------------------------------------------
+        # Name for reading housing data
+        "static_housing_data_org" = glue::glue(
+            "{static_housing_types}_housing_data_org"
+        )
+    )
+
+    #--------------------------------------------------
+    # return
+
+    return(target_names)
+}
