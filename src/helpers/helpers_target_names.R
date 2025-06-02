c1d4e0338cc9641c5a777caec66e3c15edd4ccab
Patrick Thiel <patrickthiel@outlook.de>
2025-06-02 14:41:40 +0200

    Added target object names for latest year samples

--- src/helpers/helpers_target_names.R
+++ src/helpers/helpers_target_names.R
@@ -16,17 +16,25 @@ helpers_target_names <- function() {
     target_names <- list(
         "static_housing_types" = static_housing_types,
         #--------------------------------------------------
+        # preparation housing data
         "static_housing_data_org" = glue::glue(
             "{static_housing_types}_housing_data_org"
         ),
         "static_housing_data_cleaned" = glue::glue(
             "{static_housing_types}_housing_data_cleaned"
         ),
+        #--------------------------------------------------
+        # panel Campus Files
         "static_large_cities_sampled" = glue::glue(
             "{static_housing_types}_large_cities_sampled"
         ),
         "static_panel_sampled" = glue::glue(
             "{static_housing_types}_panel_sampled"
+        ),
+        #--------------------------------------------------
+        # cross-section Campus Files
+        "static_latest_year_sampled" = glue::glue(
+            "{static_housing_types}_latest_year_sampled"
         )
     )
 
